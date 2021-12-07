5c8393a33a9e9f7e124414e72a26ee546f90df5f
Javier Ginés <jgines@webel.es>
2021-12-07 02:58:12 +0100

    Bugfix syntaxerror on Constants

--- src/aws-lambda-layer-cache/constants.js
+++ src/aws-lambda-layer-cache/constants.js
@@ -2,5 +2,5 @@ module.exports = Object.freeze({
     LOCALHOST_PORT: !isNaN(process.env.LAMBDA_LAYER_CACHE_PORT) ? parseInt(process.env.LAMBDA_LAYER_CACHE_PORT) : 8080,
     
     CACHE_CONFIG_PATH: process.env.LAMBDA_LAYER_CACHE_CONFIG_PATH ?? '/var/task/config.json',
-    CACHE_EXPIRATION_MINUTES = !isNaN(process.env.LAMBDA_LAYER_CACHE_TIMEOUT) ? parseInt(process.env.LAMBDA_LAYER_CACHE_TIMEOUT) : 60
+    CACHE_EXPIRATION_MINUTES: !isNaN(process.env.LAMBDA_LAYER_CACHE_TIMEOUT) ? parseInt(process.env.LAMBDA_LAYER_CACHE_TIMEOUT) : 60
 })
